797c51b44600150158737ee2925862230297f2be
erick <epurino@netflix.com>
2017-09-05 10:01:12 -0700

    update CTA component

--- src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -1,13 +1,28 @@
 // GENERIC SOURCE TRACKER: wc-netflix-cta
-if (typeof module === 'undefined') { module = {}; }
+if (typeof module === 'undefined') {
+    module = {};
+}
+
 module.exports = {
-  "id": "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
-  "name": "wc-netflix-cta",
-  "description": "CTA button",
-  "type": "web_components",
-  "context": "Default",
-  "state": "development",
-  "updated": 1504135752785,
-  "full_name": "NetflixAdsEng/wc-netflix-cta",
-  "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta"
+    "id"         : "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
+    "name"       : "wc-netflix-cta",
+    "description": "CTA button",
+    "type"       : "web_components",
+    "context"    : "Default",
+    "state"      : "published",
+    "updated"    : 15041357528383,
+    "full_name"  : "NetflixAdsEng/wc-netflix-cta",
+    "html_url"   : "https://github.com/NetflixAdsEng/wc-netflix-cta",
+    "version"    : "0.2",
+    "attributes" : [
+        {
+           "name": "data-dynamic-key",
+           "label": "data-dynamic-key",
+           "type": "string",
+           "required": true,
+           "defaultValue": "CTA",
+           "gwdSpecific": { 'important': true },
+           "advanced": true
+        }
+    ]
 };

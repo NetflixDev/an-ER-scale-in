a67e776fb767f7a79dd8555a32e19d1a4d04ee1a
Greg Connell <greg.connell@ff0000.com>
2017-11-14 11:34:31 -0800

    keeping the .init flag out of the commit

--- dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -13,7 +13,7 @@ module.exports = {
   "updated": 15041357528383,
   "full_name": "NetflixAdsEng/wc-netflix-cta",
   "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta",
-  "version": "[feature/dependencies 978d2d7]",
+  "version": "[feature/dependencies b2b446e]",
   "attributes": [
     {
       "name": "data-dynamic-key",

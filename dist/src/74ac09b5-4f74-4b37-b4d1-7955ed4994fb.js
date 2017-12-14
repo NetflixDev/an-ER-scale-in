5a4adf2b1776ec373b36c65a762901c37a16a947
erick <epurino@netflix.com>
2017-12-13 18:05:20 -0800

    update package

--- dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -4,25 +4,27 @@ if (typeof module === "undefined") {
 }
 
 module.exports = {
-    "id"         : "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
-    "name"       : "wc-netflix-cta",
-    "description": "CTA button",
-    "type"       : "web_components",
-    "context"    : "Default",
-    "state"      : "published",
-    "updated"    : 15041357528383,
-    "full_name"  : "NetflixAdsEng/wc-netflix-cta",
-    "html_url"   : "https://github.com/NetflixAdsEng/wc-netflix-cta",
-    "version"    : "0.2",
-    "attributes" : [
-        {
-           "name": "data-dynamic-key",
-           "label": "data-dynamic-key",
-           "type": "string",
-           "required": true,
-           "defaultValue": "CTA",
-           "gwdSpecific": { "important": true },
-           "advanced": true
-        }
-    ]
+  "id": "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
+  "name": "wc-netflix-cta",
+  "description": "CTA button",
+  "type": "web_components",
+  "context": "Default",
+  "state": "published",
+  "updated": 15041357528383,
+  "full_name": "NetflixAdsEng/wc-netflix-cta",
+  "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta",
+  "version": "[master 90e8aed]",
+  "attributes": [
+    {
+      "name": "data-dynamic-key",
+      "label": "data-dynamic-key",
+      "type": "string",
+      "required": true,
+      "defaultValue": "CTA",
+      "gwdSpecific": {
+        "important": true
+      },
+      "advanced": true
+    }
+  ]
 };

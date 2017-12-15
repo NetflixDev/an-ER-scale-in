897b44cc895debbf44d063e3977d7b0ab2ec1f46
erick <epurino@netflix.com>
2017-12-15 15:34:09 -0800

    remove /node_modules from /dist/src

--- dist/src/node_modules/@netflixadseng/pk-component-utils/src/DeviceUtils.js
+++
@@ -1,8 +0,0 @@
-window.Utils = window.Utils || {}
-
-Utils.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-
-Utils.isiOS    = /iPad|iPhone|iPod/.test(navigator.userAgent);
-Utils.isiOS9up = this.isiOS && ((navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/)[1] > 9);
-Utils.isiPad   = /iPad/.test(navigator.userAgent);
-Utils.isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);

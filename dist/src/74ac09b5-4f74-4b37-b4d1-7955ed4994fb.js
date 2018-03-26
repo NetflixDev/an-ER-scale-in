f447796ed5bb50b9653768902ac3d3ad0c5fda96
erick <epurino@netflix.com>
2018-03-26 11:25:35 -0700

    update pkg ver

--- dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/src/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -10,9 +10,9 @@ module.exports = {
   "type": "web_components",
   "context": "Default",
   "state": "published",
-  "updated": 1521136689049,
+  "updated": 1522088735604,
   "full_name": "NetflixAdsEng/wc-netflix-cta",
   "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta",
   "username": "GitHub",
-  "version": "1.1.8"
+  "version": "1.2.0"
 }

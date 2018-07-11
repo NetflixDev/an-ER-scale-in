d904a4d772bcbbced3c04691423fdfbae3683089
Greg Connell <greg.connell@ff0000.com>
2018-07-11 14:15:43 -0700

    Refactor/gwd deps (#24)
    * removes `pk-component-utils` deps from manifest.json
     - added `wc-monet-integrator` instead
    
    * copies in new lib
    adds top-level npm install on publish
    
    * dist
    
    * ensures manifest.json version matches pkg.json
    
    * tracks & dists
    
    * pipes strings into zip as binary
     - otherwise operating system guesses during unzip

--- dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -10,7 +10,7 @@ module.exports = {
   "type": "web_components",
   "context": "Default",
   "state": "published",
-  "updated": 1527806966605,
+  "updated": 1530317020950,
   "full_name": "NetflixAdsEng/wc-netflix-cta",
   "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta",
   "username": "GitHub",

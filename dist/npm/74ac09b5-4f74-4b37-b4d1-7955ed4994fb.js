1f31d3ffec61a5e8802e8d9777d66db2167b28d3
Greg Connell <greg.connell@ff0000.com>
2019-02-12 12:31:01 -0800

    updates name of component to `an-test`

--- dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -1,18 +1,18 @@
-// GENERIC SOURCE TRACKER: wc-netflix-cta
+// GENERIC SOURCE TRACKER: an-test
 if (typeof module === 'undefined') {
   module = {};
 }
 // prettier-ignore
 module.exports = {
   "id": "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
-  "name": "wc-netflix-cta",
+  "name": "an-test",
   "description": "CTA button",
-  "type": "web_components",
+  "type": "animations",
   "context": "Default",
   "state": "published",
   "updated": 1544728889095,
-  "full_name": "NetflixAdsEng/wc-netflix-cta",
-  "html_url": "https://github.com/NetflixAdsEng/wc-netflix-cta",
+  "full_name": "NetflixAdsEng/an-test",
+  "html_url": "https://github.com/NetflixAdsEng/an-test",
   "username": "GitHub",
   "version": "5.0.0",
   "minimum": "3.0.0"

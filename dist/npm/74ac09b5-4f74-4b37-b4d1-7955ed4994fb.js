2f21ac7363f5daa288676874e320a97f2b6039af
Greg Connell <greg.connell@ff0000.com>
2019-02-13 18:25:53 -0800

    CONVERTS TO ANIMATION COMPONENT

--- dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
+++ dist/npm/74ac09b5-4f74-4b37-b4d1-7955ed4994fb.js
@@ -1,18 +1,18 @@
-// GENERIC SOURCE TRACKER: an-test
+// GENERIC SOURCE TRACKER: an-endframe-fade-in
 if (typeof module === 'undefined') {
   module = {};
 }
 // prettier-ignore
 module.exports = {
   "id": "74ac09b5-4f74-4b37-b4d1-7955ed4994fb",
-  "name": "an-test",
+  "name": "an-endframe-fade-in",
   "description": "CTA button",
   "type": "animations",
   "context": "Default",
   "state": "published",
   "updated": 1544728889095,
-  "full_name": "NetflixDev/an-test",
-  "html_url": "https://github.com/NetflixDev/an-test",
+  "full_name": "NetflixDev/an-endframe-fade-in",
+  "html_url": "https://github.com/NetflixDev/an-endframe-fade-in",
   "username": "GitHub",
   "version": "5.0.0",
   "minimum": "3.0.0"

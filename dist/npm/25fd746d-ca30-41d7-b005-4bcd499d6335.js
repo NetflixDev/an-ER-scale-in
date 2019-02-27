fae830236cc4a99498ebec1a1785fd5ea9dc7bf9
Greg Connell <greg.connell@ff0000.com>
2019-02-27 10:42:46 -0800

    establishes unique scale component

---
+++ dist/npm/25fd746d-ca30-41d7-b005-4bcd499d6335.js
@@ -0,0 +1,19 @@
+// GENERIC SOURCE TRACKER: an-ER-scale-in
+if (typeof module === 'undefined') {
+  module = {};
+}
+// prettier-ignore
+module.exports = {
+  "id": "25fd746d-ca30-41d7-b005-4bcd499d6335",
+  "name": "an-ER-scale-in",
+  "description": "CTA button",
+  "type": "animations",
+  "context": "Default",
+  "state": "published",
+  "updated": 1544728889095,
+  "full_name": "NetflixDev/an-ER-scale-in",
+  "html_url": "https://github.com/NetflixDev/an-ER-scale-in",
+  "username": "GitHub",
+  "version": "1.0.0",
+  "minimum": "1.0.0"
+}

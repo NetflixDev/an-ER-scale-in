f9304a4f02dd6fd7944bd16bcd42bf0b812195d7
Derek Miranda <derek.miranda@ff0000.com>
2019-07-25 16:53:52 -0700

    refactor: makes animation backwards-compatible

--- src/creative.js
+++ src/creative.js
@@ -6,7 +6,11 @@ var Creative = function() {
     TweenLite.set(View.endFrame.background, { opacity: 0, scale: 2, delay: 0 });
     TweenLite.set(View.endFrame.tuneIn, { opacity: 0, scale: 2, delay: 0 });
     TweenLite.set(View.endFrame.ftm, { opacity: 0, scale: 2, delay: 0 });
-    TweenLite.set(View.endFrame.netflixLogo, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.netflixLogo, {
+      opacity: 0,
+      scale: 2,
+      delay: 0
+    });
     TweenLite.set(View.endFrame.cta, { opacity: 0, scale: 2, delay: 0 });
   };
 
@@ -14,12 +18,12 @@ var Creative = function() {
     console.log('Creative.play()');
 
     var scaleTime = 1.5;
-    TweenLite.to(View.endFrame.tt, scaleTime, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.background, scaleTime, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.tuneIn, scaleTime, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.ftm, scaleTime, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.netflixLogo, scaleTime, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.cta, scaleTime, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.tt, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.background, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.tuneIn, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.ftm, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.netflixLogo, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.cta, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
 
     TweenLite.delayedCall(scaleTime - 0.5, function() {
       View.endFrame.netflixLogo.play();

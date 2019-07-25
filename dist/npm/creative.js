23b9635d189e48ed47fabf1629f076feef1e7758
Derek Miranda <derek.miranda@ff0000.com>
2019-07-25 16:54:46 -0700

    chore: dist

--- dist/npm/creative.js
+++ dist/npm/creative.js
@@ -2,44 +2,28 @@ var Creative = function() {
   this.init = function() {
     console.log('Creative.init()');
 
-    TweenLite.set(View.endFrame.tt, { alpha: 0, scale: 2 });
-    TweenLite.set(View.endFrame.background, { alpha: 0, scale: 2 });
-    TweenLite.set(View.endFrame.tuneIn, { alpha: 0, scale: 2 });
-    TweenLite.set(View.endFrame.ftm, { alpha: 0, scale: 2 });
-    TweenLite.set(View.endFrame.netflixLogo, { alpha: 0, scale: 2 });
-    TweenLite.set(View.endFrame.cta, { alpha: 0, scale: 2 });
+    TweenLite.set(View.endFrame.tt, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.background, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.tuneIn, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.ftm, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.netflixLogo, {
+      opacity: 0,
+      scale: 2,
+      delay: 0
+    });
+    TweenLite.set(View.endFrame.cta, { opacity: 0, scale: 2, delay: 0 });
   };
 
   this.play = function() {
     console.log('Creative.play()');
 
     var scaleTime = 1.5;
-    TweenLite.to(View.endFrame.tt, scaleTime, { alpha: 1, scale: 1, delay: 0 });
-    TweenLite.to(View.endFrame.background, scaleTime, {
-      alpha: 1,
-      scale: 1,
-      delay: 0
-    });
-    TweenLite.to(View.endFrame.tuneIn, scaleTime, {
-      alpha: 1,
-      scale: 1,
-      delay: 0
-    });
-    TweenLite.to(View.endFrame.ftm, scaleTime, {
-      alpha: 1,
-      scale: 1,
-      delay: 0
-    });
-    TweenLite.to(View.endFrame.netflixLogo, scaleTime, {
-      alpha: 1,
-      scale: 1,
-      delay: 0
-    });
-    TweenLite.to(View.endFrame.cta, scaleTime, {
-      alpha: 1,
-      scale: 1,
-      delay: 0
-    });
+    TweenLite.fromTo(View.endFrame.tt, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.background, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.tuneIn, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.ftm, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.netflixLogo, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.cta, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
 
     TweenLite.delayedCall(scaleTime - 0.5, function() {
       View.endFrame.netflixLogo.play();

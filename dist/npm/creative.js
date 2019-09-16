64d0023c81f205e9b6399fee0755a305026e689e
Derek Miranda <derek.miranda@ff0000.com>
2019-09-16 16:32:17 -0700

    Tunein/ftm back to headline
    This reverts commit 3bafb9178ae63aba7c01be6cb923f88c656a68d7.

--- dist/npm/creative.js
+++ dist/npm/creative.js
@@ -4,8 +4,7 @@ var Creative = function() {
 
     TweenLite.set(View.endFrame.tt, { opacity: 0, scale: 2, delay: 0 });
     TweenLite.set(View.endFrame.background, { opacity: 0, scale: 2, delay: 0 });
-    TweenLite.set(View.endFrame.tuneIn, { opacity: 0, scale: 2, delay: 0 });
-    TweenLite.set(View.endFrame.ftm, { opacity: 0, scale: 2, delay: 0 });
+    TweenLite.set(View.endFrame.headline, { opacity: 0, scale: 2, delay: 0 });
     TweenLite.set(View.endFrame.netflixLogo, {
       opacity: 0,
       scale: 2,
@@ -20,8 +19,7 @@ var Creative = function() {
     var scaleTime = 1.5;
     TweenLite.fromTo(View.endFrame.tt, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
     TweenLite.fromTo(View.endFrame.background, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.fromTo(View.endFrame.tuneIn, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
-    TweenLite.fromTo(View.endFrame.ftm, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
+    TweenLite.fromTo(View.endFrame.headline, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
     TweenLite.fromTo(View.endFrame.netflixLogo, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
     TweenLite.fromTo(View.endFrame.cta, scaleTime, { opacity: 0, scale: 2 }, { opacity: 1, scale: 1, delay: 0 });
 
@@ -36,4 +34,3 @@ Creative.zoomDuration = 1.7;
 
 // how much to scale the keyart intro frame
 Creative.zoomAmount = 5;
-
